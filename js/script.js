a324b83606c1115b8545f214a6c7cf1ca0ede62c
Leonid <troitskylv@gmail.com>
2021-03-14 23:07:22 +0300

    DZ2

--- js/script.js
+++ js/script.js
@@ -10,13 +10,34 @@ const personalMovieDB = {
     privat: false,
 };
 
-const answer1 = prompt("Один из последних просмотренных фильмов?", ""),
-      answer2 = prompt("На сколько оцените его?", ""),
+// const answer1 = prompt("Один из последних просмотренных фильмов?", ""),
+//       answer2 = prompt("На сколько оцените его?", ""),
 
-      answer3 = prompt("Один из последних просмотренных фильмов?", ""),
-      answer4 = prompt("На сколько оцените его?", "");
+//       answer3 = prompt("Один из последних просмотренных фильмов?", ""),
+//       answer4 = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[answer1] = answer2;
-personalMovieDB.movies[answer3] = answer4;
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?", "");
+
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+// personalMovieDB.movies[answer1] = answer2;
+// personalMovieDB.movies[answer3] = answer4;
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало филььмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log("Вы клссичесыкий зритель");
+} else {
+    console.log("Вы киноман");
+}
 
 console.log(personalMovieDB);
--- script2.js
+++ script2.js
@@ -4,8 +4,25 @@ if (4) {
     console.log('Ok!');
 } else {
     console.log('Error');
-}\
+}
 
 const num = 50;
 
-(num === 50) ? console.log('Ok!') : console.log('Error');
+// (num === 50) ? console.log('Ok!') : console.log('Error');
+
+
+
+switch (num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('В точку!');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
